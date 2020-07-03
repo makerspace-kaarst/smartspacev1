c602a3778de2238c51dc7ffcc5cf694f52faaf1b
Stephan Wiechers <stephan.wiechers04@gmail.com>
2020-07-03 14:16:28 +0200

    themeing engine v0.1

--- themes/slate/static/js/linker.js
+++ themes/slate/static/js/linker.js
@@ -2,8 +2,6 @@ var links = document.links;
 for (var i = 0, linksLength = links.length; i < linksLength; i++) {
   if (links[i].hostname != window.location.hostname) {
     links[i].target = '_blank';
-    console.log(links[i].innerHTML);
     links[i].innerHTML = links[i].innerHTML.replace("class=","target='_blank' class=")
   }
-  console.log(links[i].hostname);
 }
---
+++ themes/slate/static/js/theme_engine.js
@@ -0,0 +1,31 @@
+var THEMES = {
+  'light':`
+  --main-background-color:#fff;
+  --font-color:#000;
+  --link-color: #5252ff;
+  --table-main:#999;
+  --table-even:#b9b9b9;
+  --table-odd: #ddd;`,
+  'dark':`
+  --main-background-color:#444;
+  --font-color:#fff;
+  --link-color: #568de6;
+  --table-main:#111;
+  --table-even:#222;
+  --table-odd: #333;`
+}
+
+function loadTheme(theme) {
+  let vars = THEMES[theme].split('\n');
+  for (var i = 0; i < vars.length; i++) {
+    let splits = vars[i].split(':');
+    if(splits.length == 2){
+      for(let i = 0;i<20;i++){
+        splits[0] = splits[0].replace(' ','')
+        splits[1] = splits[1].replace(' ','')
+      }
+      console.log(splits);
+      document.documentElement.style.setProperty(splits[0].replace(' ',''), splits[1].replace(';',''));
+    }
+  }
+}
